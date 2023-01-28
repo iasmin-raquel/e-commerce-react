858159fa4e0082ec6348266fde9a80cf9ca69f79
Iasmin <iasminraquelsou@gmail.com>
2023-01-27 22:56:16 -0300

    styling cart

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { ShopContext } from '../../context/shop-context'
 
 export const CartItem = (props) => {
-  const  {productName, price, productImage} = props.data
+  const  {id, productName, price, productImage} = props.data
+  const { cartItems } = useContext(ShopContext)
 
   return (
     <div className='cartItem' >
@@ -11,6 +13,11 @@ export const CartItem = (props) => {
           <b>{productName}</b>
         </p>
         <p> $ {price} </p>
+        <div className='countHandler'> 
+          <button>-</button>
+          <input value={cartItems[id]}/>
+          <button>+</button>
+        </div>
       </div>
     </div>
   )
